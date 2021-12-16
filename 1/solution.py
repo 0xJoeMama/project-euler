f4ccc6a02f5385515a5257d6daca49abf88d1a1e
0xJoeMama <0xjoemama@gmail.com>
2021-12-16 22:11:19 +0200

    That was a bit inefficient now wasn't it

--- 1/solution.py
+++ 1/solution.py
@@ -1,9 +1,15 @@
 # Problem 1
 
+end = 1000
 sum = 0
 
-for i in range(0, 1000):
-    if (i % 3 == 0) or (i % 5 == 0):
+for i in range(3, end, 3):
+    if i % 5 != 0:
         sum += i
+
+
+for i in range(5, end, 5):
+    sum += i
+
 
 print(sum)
