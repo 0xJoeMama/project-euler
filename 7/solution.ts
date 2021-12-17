312b72f70aada9a51fd961bd7dbe5bb932cacb09
0xJoeMama <0xjoemama@gmail.com>
2021-12-17 20:49:11 +0200

    Semicolons; in javascript; nah

--- 7/solution.ts
+++ 7/solution.ts
@@ -8,11 +8,11 @@ const primes_before: number[] = []
 
 for (let i = 0, last = 2; i < max_prime_index; i++) {
     while (primes_before.some((prime) => last % prime == 0)) {
-        last++;
+        last++
     }
 
     primes_before.push(last)
 }
 
-const result: number = primes_before[primes_before.length - 1];
+const result: number = primes_before[primes_before.length - 1]
 console.log(result)
